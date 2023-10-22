3a8b909eea3d26e96424f0fe9da2d0dcffcc4d87
vishrudh raj <vishrudh.shrinivas@gmail.com>
2023-10-22 22:36:49 +0530

    Modified a typo

--- controller/projects/projects.controller.ts
+++ controller/projects/projects.controller.ts
@@ -3,12 +3,12 @@ import Organisation from "../../models/organization.model";
 import Project from "../../models/project.model";
 
 const getAllProjects = expressAsyncHandler(async (req: any, res: any) => {
-  const organisations = await Project.find();
+  const projects = await Project.find();
 
   return res.status(201).json({
     status: "success",
     data: {
-      organisations,
+      projects,
     },
   });
 });
